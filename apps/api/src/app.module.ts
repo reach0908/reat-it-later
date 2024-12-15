97aa61caaa30774a6f4604ced898857914a41611
reach0908 <wlsghkim0908@gmail.com>
2024-12-15 13:14:26 +0900

    chore(package): next auth 라이브러리 설치

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [AuthModule, UserModule],
